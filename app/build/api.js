404dc67a63890ec296142fe065bf98b0fc03d515
Chris Stadler <chrisstadler@gmail.com>
2016-01-06 16:06:41 -0500

    Mean sentiment for comments of each story

--- app/build/api.js
+++ app/build/api.js
@@ -3,9 +3,9 @@ var Firebase = require('firebase');
 var base_url = "https://hacker-news.firebaseio.com/v0/";
 
 var api = {
-    base_url: base_url,
+    root_ref: new Firebase(base_url),
     topstories: function(limit, callback) {
-        var ref = new Firebase(base_url + "topstories/");
+        var ref = this.root_ref.child("topstories");
         ref.limitToFirst(limit).on("child_added", function(story_id_obj) {
             var id = story_id_obj.val();
             // get story
@@ -16,25 +16,45 @@ var api = {
         }.bind(this));
     },
     item: function(id, callback) {
-        var ref = new Firebase(base_url + "item/" + id);
+        var ref = this.root_ref.child("item/" + id);
 
         ref.once("value", function(snapshot) {
             var item = snapshot.val();
 
-            if (item.hasOwnProperty("kids") && item.kids.length > 0) {
-                item.kids.forEach(function(id,i) {
-                    this.item(id, function(kid) {
-                        item.kids[i] = kid;
-                    });
-                }.bind(this));
-            }
+            // if (item.hasOwnProperty("kids") && item.kids.length > 0) {
+            //     item.kids.forEach(function(id,i) {
+            //         this.item(id, function(kid) {
+            //             item.kids[i] = kid;
+            //             //descendant_callback(kid)
+            //         });
+            //     }.bind(this));
+            // }
 
             callback(item);
 
         }.bind(this));
     },
-    kids: function(id) {
-        return new Firebase(base_url + "item/" + id + "/kids/");
+    all_descendants: function(parent, callback) {
+        var ref = this.root_ref.child("item/" + parent.id).child("kids");
+
+        ref.on("child_added", function(id_obj) {
+            var id = id_obj.val();
+            // get object corresponding to id
+            this.item(id, function(item) {
+
+                callback(item);
+
+                // add item to parent comments
+                if (! parent.hasOwnProperty("comments")) {
+                    parent.comments = [];
+                }
+                parent.comments.push(item);
+
+                // recurse on item descendants
+                this.all_descendants(item, callback);
+
+            }.bind(this));
+        }.bind(this));
     }
 };
 module.exports = api;
--- app/src/components/comments_summary.jsx
+++ app/src/components/comments_summary.jsx
@@ -1,24 +1,62 @@
-var React = require('react');
+var React = require('react'),
+    sentiment = require('sentiment'),
+    api = require("../api.js");
 
 var CommentsSummary = React.createClass({
 
+    getInitialState: function() {
+        return {sentiments: []};
+    },
+
+    componentWillMount: function() {
+        api.all_descendants(this.props.story, function(comment) {
+
+            if (typeof comment === "undefined") {
+                debugger;
+            }
+            
+            if (! comment.hasOwnProperty("deleted") || comment.deleted === false) {
+                var s_comp = sentiment(comment.text).comparative;
+                // update state
+                this.setState(function(previousState, currentProps) {
+                    var sentiments = previousState.sentiments.slice();
+                    sentiments.push(s_comp);
+                    return {
+                        sentiments: sentiments
+                    };
+                });
+            }
+
+        }.bind(this));
+    },
+
     render: function() {
-        var count = this.props.story.descendants;
-        // each_item(this.props.comments, function(item) {
-        //     count += 1;
-        // });
-        return <div>{count} comments</div>;
+        var sum;
+        var mean = 0;
+        var n_comments = this.state.sentiments.length;
+
+        if (n_comments > 0) {
+            sum = this.state.sentiments.reduce(function(sum, current) {
+                return sum + current;
+            }, 0);
+
+            mean = sum / this.state.sentiments.length;
+        }
+
+        return <div>{this.props.story.descendants} comments {mean}</div>;
     }
 
 });
 
-var each_item = function(items, callback) {
-    items.forEach(function(item) {
-        callback(item);
-        if (item.hasOwnProperty("kids")) {
-            each_item(item.kids, callback);
-        }
-    })
-}
+// var each_comment = function(item, callback) {
+//     if (typeof item !== 'undefined' && item !== null) {
+//         callback(item);
+//         if (item.hasOwnProperty("comments")) {
+//             item.comments.forEach(function(comment) {
+//                 each_comment(comment, callback);
+//             });
+//         }
+//     }
+// }
 
 module.exports = CommentsSummary;
