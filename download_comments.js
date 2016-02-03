2c04b4b315e1ef4dbc962db7f2525e87bef03e41
Chris Stadler <chrisstadler@gmail.com>
2016-02-03 11:51:28 -0500

    Switch to using request with callbacks

--- download_comments.js
+++ download_comments.js
@@ -2,41 +2,57 @@ var fs = require('fs'),
     api = require("./json_api.js"),
     stringify = require('csv-stringify');
 
-var limit = 100,
-    filename = "comments.csv",
-    columns = {
-        id: 'id',
-        by: 'by',
-        text: 'text'
-    },
-    options = {
-        header: true,
-        columns: columns
-    };
+var limit = 50,
+    filename = "comments.csv";
 
-//var csv_stream = fs.createWriteStream(filename);
+var headers = ['id', 'by', 'text'];
 
-var writeComments = function(comments) {
-    stringify(comments, options, function(err, output) {
-        if (err) {
-            console.log(err);
-        }
-        fs.writeFile(filename, output, function(err) {
+var csv_stream = fs.createWriteStream(filename);
+
+var write_comment = function(comment) {
+    var columns = headers.map(function(h) {
+        return comment[h];
+    });
+    stringify([columns], {header: false}, function(err, output) {
             if (err) {
                 console.log(err);
+            } else {
+                csv_stream.write(output);
             }
-            console.log("csv created");
-        });
-    });
+       });
 };
 
-console.log("getting comments of top " + limit + " stories");
-api.comments_from_topstories(limit)
-    .then(function(comments) {
-        console.log(comments.length + " comments found");
-        writeComments(comments);
-        return comments;
-    });
+var n = 0;
+api.each_comment_from_topstories(limit, function(comment) {
+    write_comment(comment);
+    process.stdout.write(".");
+    n += 1;
+    if (n % 50 === 0) {
+        process.stdout.write(n + "");
+    }
+});
+
+// var writeComments = function(comments) {
+//     stringify(comments, options, function(err, output) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         fs.writeFile(filename, output, function(err) {
+//             if (err) {
+//                 console.log(err);
+//             }
+//             console.log("csv created");
+//         });
+//     });
+// };
+
+// console.log("getting comments of top " + limit + " stories");
+// api.comments_from_topstories(limit)
+//     .then(function(comments) {
+//         console.log(comments.length + " comments found");
+//         writeComments(comments);
+//         return comments;
+//     });
 
 // api.get_item(5).then(function(o) {
 //     console.log(o);
--- json_api.js
+++ json_api.js
@@ -1,80 +1,64 @@
-var rp = require('request-promise'),
+var req = require('request'),
     Promise = require('bluebird');
 
 var base_url = "https://hacker-news.firebaseio.com/v0/";
 
 
-var request = function(url) {
+var request = function(url, callback) {
     var options = {
         uri: url,
-        headers: {'User-Agent': 'Request-Promise'},
+        headers: {'User-Agent': 'Request'},
         json: true
     };
 
-    return rp(options)
-        .catch(function(e) {
-            process.stdout.write("x");
-            return true;
-        });
-};
-
-var topstories = function(limit) {
-    var url = base_url + "topstories.json";
-
-    return request(url).then(function(ids) {
-        return ids.slice(0, limit);
+    return req(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            callback(body);
+        }
     });
 };
 
-var get_item = function(id) {
+var topstories = function(limit, callback) {
+    var url = base_url + "topstories.json";
+
+    request(url, function(ids) {
+        callback(ids.slice(0, limit));
+    });
+};
+
+var get_item = function(id, callback) {
     var url = base_url + "item/" + id + ".json";
-    process.stdout.write(".");
-    return request(url);
+    request(url, callback);
 };
 
 // get an item and all it's kids
-var all_descendants = function(id) {
-    return get_item(id)
-        .then(function(item) {
-            var items;
+var all_descendants = function(id, callback) {
+    get_item(id, function(item) {
+            // apply callback to item
+            callback(item);
+
+            // recurse on descendants
             if (item.hasOwnProperty("kids")) {
-                items = descendants_from_ids(item.kids)
-                    .then(function(items) {
-                        if (item.type === "comment") {
-                            // need to add item to the array so that it is included
-                            items.push(item);
-                        }
-                        return items;
-                    });
-            } else {
-                items = item;
+                descendants_from_ids(item.kids, callback);
             }
-            return items;
         });
 };
 
-var descendants_from_ids = function(ids) {
-    descendants = ids.map(function(id) {
-        return all_descendants(id);
+var descendants_from_ids = function(ids, callback) {
+    ids.forEach(function(id) {
+        all_descendants(id, callback);
     });
-
-    return Promise.all(descendants)
-        .then(function(descendants) {
-            var flattened = [].concat.apply([], descendants);
-            return flattened;
-        });
 };
 
-var comments_from_topstories = function(limit) {
-    return topstories(limit)
-        .then(function(ids) {
-            return descendants_from_ids(ids);
-        });
+var each_comment_from_topstories = function(limit, callback) {
+    topstories(limit, function(ids) {
+        descendants_from_ids(ids, callback);
+    });
 };
 
 module.exports = {
     get_item: get_item,
     topstories: topstories,
     all_descendants: all_descendants,
-    comments_from_topstories: comments_from_topstories
+    each_comment_from_topstories: each_comment_from_topstories
 };
