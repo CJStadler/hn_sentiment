2218a74784ef12b4f796cb626eb46afd33433264
Chris Stadler <chrisstadler@gmail.com>
2016-02-03 15:22:24 -0500

    Fix bugs and set max pool size to prevent errors
    Successfully downloaded all comments from topstories

--- download_comments.js
+++ download_comments.js
@@ -2,17 +2,13 @@ var fs = require('fs'),
     api = require("./json_api.js"),
     stringify = require('csv-stringify');
 
-var limit = 50,
-    filename = "comments.csv";
-
-var headers = ['id', 'by', 'text'];
+var limit = 500,
+    filename = "comments.csv",
+    headers = ['id', 'by', 'parent', 'time', 'text'];
 
 var csv_stream = fs.createWriteStream(filename);
 
-var write_comment = function(comment) {
-    var columns = headers.map(function(h) {
-        return comment[h];
-    });
+var write_line = function(columns) {
     stringify([columns], {header: false}, function(err, output) {
             if (err) {
                 console.log(err);
@@ -22,55 +18,23 @@ var write_comment = function(comment) {
        });
 };
 
+// write headers
+write_line(headers);
+
+var write_comment = function(comment) {
+    var columns = headers.map(function(h) {
+        return comment[h];
+    });
+    write_line(columns);
+};
+
+console.log("getting comments for " + limit + " topstories");
 var n = 0;
 api.each_comment_from_topstories(limit, function(comment) {
     write_comment(comment);
     process.stdout.write(".");
     n += 1;
-    if (n % 50 === 0) {
+    if (n % 100 === 0) {
         process.stdout.write(n + "");
     }
 });
-
-// var writeComments = function(comments) {
-//     stringify(comments, options, function(err, output) {
-//         if (err) {
-//             console.log(err);
-//         }
-//         fs.writeFile(filename, output, function(err) {
-//             if (err) {
-//                 console.log(err);
-//             }
-//             console.log("csv created");
-//         });
-//     });
-// };
-
-// console.log("getting comments of top " + limit + " stories");
-// api.comments_from_topstories(limit)
-//     .then(function(comments) {
-//         console.log(comments.length + " comments found");
-//         writeComments(comments);
-//         return comments;
-//     });
-
-// api.get_item(5).then(function(o) {
-//     console.log(o);
-// });
-
-// console.log("getting topstories...");
-// // collect all comments descending from topstories
-// api.topstories(limit, refCollector, function(story_id) {
-//     // CALLBACK IS NEVER FIRING
-//     console.log("got story: " + story_id);
-//     api.all_descendants(story_id, refCollector, writeComment);
-// });
-
-// api.item(1, refCollector, function(item) {
-//     console.log("got item: " + item);
-// });
-
-// turn off all refs
-// refs.forEach(function(ref) {
-//     ref.off();
-// });
--- json_api.js
+++ json_api.js
@@ -3,17 +3,18 @@ var req = require('request'),
 
 var base_url = "https://hacker-news.firebaseio.com/v0/";
 
+var json_req = req.defaults({
+    headers: {'User-Agent': 'Request'},
+    json: true,
+    pool: {maxSockets: 10}
+}); // if pool is too large then there are server errors (and high memory usage)
 
 var request = function(url, callback) {
-    var options = {
-        uri: url,
-        headers: {'User-Agent': 'Request'},
-        json: true
-    };
-
-    return req(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
+    json_req(url, function (error, response, body) {
+        if (!error && response.statusCode == 200 && typeof body !== "undefined" && body !== null) {
             callback(body);
+        } else if (error) {
+            process.stdout.write(error.code);
         }
     });
 };
@@ -22,7 +23,10 @@ var topstories = function(limit, callback) {
     var url = base_url + "topstories.json";
 
     request(url, function(ids) {
-        callback(ids.slice(0, limit));
+        if (limit < ids.length) {
+            ids = ids.slice(0, limit);
+        }
+        callback(ids);
     });
 };
 
@@ -34,8 +38,10 @@ var get_item = function(id, callback) {
 // get an item and all it's kids
 var all_descendants = function(id, callback) {
     get_item(id, function(item) {
-            // apply callback to item
-            callback(item);
+            // apply callback to comment
+            if (item.type === "comment") { // what about if deleted?
+                callback(item);
+            }
 
             // recurse on descendants
             if (item.hasOwnProperty("kids")) {
